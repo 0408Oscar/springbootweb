8e11124a85f4d7528094a5b1a1bbe4ee16230fbc
0408Oscar <okch622@gmail.com>
2019-10-07 01:07:11 +0800

    create excel and zip it

--- complete/src/test/java/hello/GenerateExcel.java
+++ complete/src/test/java/hello/GenerateExcel.java
@@ -1,7 +1,8 @@
-package application.controller;
+package hello;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
@@ -13,7 +14,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import javax.servlet.http.HttpServletResponse;
+
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.FillPatternType;
@@ -22,41 +23,23 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
-public class DownloadFileController {
-
+public class GenerateExcel {
+	
 	private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss");
 	private static String fileName = "excel_" + sdf.format(new Date()) + ".xlsx";
 	private static String zipName = "zip_" + sdf.format(new Date()) + ".zip";
 
-	@RequestMapping(value = "/downloadpage")
-	public String download(
-			@RequestParam(name = "downloadpage", required = false, defaultValue = "default downloadpage") String name,
-			Model model) {
-		model.addAttribute("downloadpage", name);
-		return "/content/downloadpage";
-	}
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws Exception, IOException {
 
-	@RequestMapping(value = "/downloadExcel", method = RequestMethod.GET)
-	public String downloadExcel(HttpServletResponse resp) throws Exception {
+		System.out.println("a: " + System.getProperty("user.dir"));
+		System.out.println("b: " + System.getProperty("user.home"));
+		System.out.println();
 
-		Path zipFile = GenerateExcel();
+		Date start = new Date();
+		System.out.println("Start at : " + start);
 
-		resp.setContentType(null);
-
-		// delete the zip file finally
-		// Files.deleteIfExists(zipfilePath);
-
-		return "/content/downloadpage";
-	}
-
-	private Path GenerateExcel() throws Exception {
 		SXSSFWorkbook wb = new SXSSFWorkbook(1); // keep 100 rows in memory, exceeding rows will be flushed to disk
 		Sheet sh = wb.createSheet();
 
@@ -85,38 +68,56 @@ public class DownloadFileController {
 		out.close();
 		// dispose of temporary files backing this workbook on disk
 		wb.dispose();
+		Date end = new Date();
+		System.out.println("End at " + end);
+
+		System.out.println("Takes  " + (end.getTime() - start.getTime()));
 
 		Path file = Paths.get(System.getProperty("user.dir") + File.separator + fileName);
-		Path zipFilePath = ZipSourceFile(file);
 
-		// return to download
-		return zipFilePath;
+		System.out.println("1: " + (file.getFileName()));
+		System.out.println("2: " + (file.getFileSystem()));
+		System.out.println("3: " + (file.getName(1)));
+		System.out.println("4: " + (file.getParent()));
+		System.out.println("5: " + (file.getName(2)));
+		System.out.println("6: " + (file.getRoot()));
+
+		System.out.println("7: " + (file.toString()));
+		System.out.println("8: " + (file.toAbsolutePath()));
+		System.out.println("9: " + (file.toRealPath()));
+		System.out.println("10: " + (file.toUri()));
+		System.out.println("11: " + (file.spliterator()));
+		System.out.println("12: " + (file.getRoot()));
+
+		Map<String, String> env = new HashMap<>();
+		// Create the zip file if it doesn't exist
+		env.put("create", "true");
+
+		URI uri = URI.create("jar:" + file.getParent().toUri() + "/" + zipName);
+
+		try (FileSystem zipfs = FileSystems.newFileSystem(uri, env)) {
+			Path sourcefile = file;
+			Path pathInZipfile = zipfs.getPath(file.getFileName().toString());
+			// Copy a file into the zip file
+			Path copy = Files.copy(sourcefile, pathInZipfile, StandardCopyOption.REPLACE_EXISTING);
+			
+			System.out.println("13: " + (copy.toString()));
+			System.out.println("13a: " + (copy.toAbsolutePath()));
+			System.out.println("14: " + (sourcefile.toString()));
+			System.out.println("14a: " + (sourcefile.toAbsolutePath()));
+			System.out.println("15: " + (pathInZipfile.toString()));
+			System.out.println("16: " + (zipfs.toString()));
+			
+			Files.deleteIfExists(sourcefile);
+		}
+
 	}
 
-	private void SetCellStyle(SXSSFWorkbook wb, Row row, Cell cell) {
+	private static void SetCellStyle(SXSSFWorkbook wb, Row row, Cell cell) {
 		CellStyle style = wb.createCellStyle();
 		style.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
 		style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 		cell.setCellStyle(style);
 	}
 
-	private Path ZipSourceFile(Path file) throws Exception {
-		Map<String, String> env = new HashMap<>();
-		// Create the zip file if it doesn't exist
-		env.put("create", "true");
-
-		URI uri = URI.create("jar:" + file.getParent().toUri() + "/" + zipName);
-		Path zipFilePath;
-		try (FileSystem zipfs = FileSystems.newFileSystem(uri, env)) {
-			Path sourcefile = file;
-			Path pathInZipfile = zipfs.getPath(file.getFileName().toString());
-			// Copy a file into the zip file
-			Files.copy(sourcefile, pathInZipfile, StandardCopyOption.REPLACE_EXISTING);
-
-			Files.deleteIfExists(sourcefile);
-			zipFilePath = Paths.get(zipfs.toString());
-		}
-
-		return zipFilePath;
-	}
 }
